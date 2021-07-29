32fdcc5f1a2ba1de2baab76b50d209ad0e669cce
Alberto Beiztegui Casado <albertobeiz@gmail.com>
2021-07-29 20:48:45 +0200

    packages

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -3,16 +3,27 @@
 import { Command } from 'commander';
 import shell from 'shelljs';
 
+import { SupoServer } from "@supojs/core";
+
 const program = new Command();
 
 program.command('create <name>')
-  .description('start')
+  .description('Create a new SupoJS project')
   .option('-t, --template <template>', 'Using a template project', 'hello-world')
   .action((name, options) => {
-    console.log(name, options);
     shell.exec('git clone --quiet https://github.com/supojs/supojs > /dev/null')
     shell.exec(`mv supojs/examples/${options.template} ${name}`)
     shell.exec(`rm -rf supojs`)
+    console.log('Project created! Run it with:');
+    console.log(`cd ${name}`);
+    console.log(`supojs serve`);
+  });
+
+  program.command('serve')
+  .description('Serve')
+  .action((name) => {
+    console.log('Starting server...');
+    const server = new SupoServer(process.cwd());
   });
 
 program.parse(process.argv);
